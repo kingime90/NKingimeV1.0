255ce5e22f5953b6cd066ae2cc3631108d45e022
kingime90 <kingime90@163.com>
2018-02-28 14:51:50 +0800

    NKingime.Core

---
+++ src/NKingime.Core/Data/IdentifyBase.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using NKingime.Core.Utility;
+
+namespace NKingime.Core.Data
+{
+    /// <summary>
+    /// 唯一标识数据实体基类。
+    /// </summary>
+    /// <typeparam name="TKey"></typeparam>
+    public abstract class IdentifyBase<TKey> : IIdentify<TKey>
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="isNew">是否新增。</param>
+        public IdentifyBase(bool isNew = true)
+        {
+            if (!isNew)
+                return;
+            //
+            var type = typeof(TKey).FullName;
+            switch (type)
+            {
+                case "System.String":
+                    Id = TypeConvertUtil.CastTo<TKey>(Guid.NewGuid().ToString());
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// 主键ID。
+        /// </summary>
+        public TKey Id { get; set; }
+    }
+}
---
+++ src/NKingime.Core/Extension/TypeExtension.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace NKingime.Core.Extension
+{
+    /// <summary>
+    /// 类型扩展方法。
+    /// </summary>
+    public static class TypeExtension
+    {
+        /// <summary>
+        /// 判断类型是否为Nullable类型。
+        /// </summary>
+        /// <param name="type"> 要处理的类型。</param>
+        /// <returns> 是返回True，不是返回False。</returns>
+        public static bool IsNullableType(this Type type)
+        {
+            return ((type != null) && type.IsGenericType) && (type.GetGenericTypeDefinition() == typeof(Nullable<>));
+        }
+
+        /// <summary>
+        /// 通过类型转换器获取Nullable类型的基础类型。
+        /// </summary>
+        /// <param name="type">要处理的类型对象。</param>
+        /// <returns> </returns>
+        public static Type GetUnNullableType(this Type type)
+        {
+            if (IsNullableType(type))
+            {
+                var nullableConverter = new NullableConverter(type);
+                return nullableConverter.UnderlyingType;
+            }
+            return type;
+        }
+    }
+}
